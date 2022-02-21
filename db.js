3140d5a1b1889086b698281c4c39606066858410
Will <twitchifygamers@gmail.com>
2022-02-21 21:10:57 +0000

    Added cheddar to fakedb

--- db.js
+++ db.js
@@ -2,12 +2,21 @@ const fakeDatabase = {
     1: {
         id: 1,
         name: 'Parmesan',
-        description: 'Its just a cheese!',
-        origin: 'Africa',
+        description: 'Parmesan is an Italian hard, granular cheese produced from cow\'s milk and aged at least 12 months. It is named after the producing areas, the provinces of Reggio Emilia, Parma, the part of Bologna west of the Reno, and Modena; and the part of Mantua on the right/south bank of the Po.',
+        origin: 'Unknown',
         score: 5,
         legal: true,
         searches: 50000,
     },
+    2: {
+        id: 2,
+        name: 'Cheddar',
+        description: 'Cheddar cheese (or simply cheddar) is a relatively hard, off-white sometimes sharp-tasting, natural cheese.',
+        origin: 'Unknown',
+        score: 3,
+        legal: true,
+        searches: 69000
+    }
 }
 
 module.exports = fakeDatabase;
